b809b7ecbb0d53c1304f6e8b2b85e0c92a386a7c
welaughindoors <welaughindoors@live.com>
2023-02-11 22:51:52 -0600

    init

---
+++ Source/EmberALS/EmberALSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EmberALSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EMBERALS_API AEmberALSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
